df4458161b16d63d0113c0a4912c5f66d35c3f3d
ShaneTheBro <50725203+ShaneTheBro@users.noreply.github.com>
2019-09-16 21:23:55 -0400

    Finished quiz

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
-/* const bills = [140, 45, 270];
+function tipCalc(bill) {
+  let percentage;
+  if (bill < 50) {
+    percentage = 0.2;
+  } else if (50 <= bill < 200) {
+    percentage = 0.15;
+  } else {
+    percentage = 0.1;
+  }
+  return percentage * bill;
+}
 
-function tipCalc(bills){
-    if (bills < 50){
-        return .2
-    }
-    if (50 < bills < 200){
-        return .15
-    }
-    if (bills > 200){
-        return .1
-    }
-    }
-    let tips = [tipCalc(bills[0]) * bills[0], tipCalc(bills[1]) * bills[1], tipCalc(bills[2]) * bills[2]]
-    console.log(tips) */
+const bills = [140, 45, 270];
 
-/* const tips = ['.2 * 140', '.2 * 45', '.2 * 270'];
-const total = [tips[0] + 140, tips[1] + 45, tips[2] + 270];
- */
+const tips = [tipCalc(bills[0]), tipCalc(bills[1]), tipCalc(bills[2])];
 
- function tipCalc(bill){
-     let percentage; 
-     if (bill < 50) {
-         percentage = .2;
-     } else if (50 <= bill < 200){
-         percentage = .15;
-     } else {
-         percentage = .1;
-     }
-     return percentage * bill;
- }
+console.log(`the tips are ${tips}`);
 
- const bills = [140, 45, 270];
+const totals = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
+console.log(`The totals are ${totals}`);
 
- const tips = [tipCalc(bills[0]), tipCalc(bills[1]), tipCalc(bills[2])];
+function bigEnough(totals){
+    return totals > 100;
+}
 
-console.log(tips);
-
-const 
+const copy = totals.filter(bigEnough);
+console.log(`The totals over $100 are ${copy}`);
